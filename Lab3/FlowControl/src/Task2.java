45412623f55fd6d3ea54ee0f2d3098f7a5e6ff83
voloshmiak <voloshenkoyolmishayol@gmail.com>
2024-09-27 01:08:48 +0300

    Edited Main.java

--- Lab3/FlowControl/src/Task2.java
+++ Lab3/FlowControl/src/Task2.java
@@ -1,26 +1,18 @@
 public class Task2 {
 
     public static void main(String[] args) {
-        printResults(0, 0);
-        printResults(0, 1);
-        printResults(1, 0);
-        printResults(1, 1);
-        printResults(2, 1);
-        printResults(1, 2);
-        printResults(2, -10);
-        printResults(20, 20);
-        printResults(21, 21);
-        printResults(1, 10);
-        printResults(-1, 10);
-        printResults(0, Double.MAX_VALUE);
-        printResults(1, Double.MAX_VALUE);
-        printResults(1, Double.NaN);
+        printResults(4, 5); // Очікується коректний результат (l непарне)
+        printResults(4, 6); // Очікується коректний результат (l парне)
+        printResults(-1, 5); // Очікується Nan через sqrt(t)
+        printResults(4, 0); // Очікується результат 0
+        printResults(2, 100000); // Очікується коректний результат
+        printResults(Double.NaN, 3); // Очікується NaN
     }
 
     public static double bxx(double t, double l) {
         double sum = 0;
-        for (int i = 1; i <= l; i++) {
-            if (l % 2 == 1) {
+        for (int i = 1; i <= l; i++) { // Сума значень від i=1 до l
+            if (l % 2 == 1) { // Перевірка на парність
                 sum += Math.sqrt(t* i);
             } else {
                 sum += (l / Math.sqrt(t));
@@ -30,7 +22,7 @@ public class Task2 {
     }
 
     static void printResults(double t, double l) {
-        System.out.print("t: " + t + " l: " + l + " ");
+        System.out.println("t: " + t + " l: " + l + " ");
         System.out.println("Result: " + bxx(t, l));
     }
 }
--- Lab3/FlowControl/src/Task3.java
+++ Lab3/FlowControl/src/Task3.java
@@ -1,36 +1,32 @@
 public class Task3 {
 
     public static void main(String[] args) {
-        printResults(0);
-        printResults(2);
-        printResults(1);
-        printResults(-10);
-        printResults(6);
-        printResults(7);
-        printResults(12);
-        printResults(78);
-        printResults(0);
-        printResults(Double.MAX_VALUE);
-        printResults(Double.NaN);
+        printResults(0.01); // Очікується коректний результат
+        printResults(0); // Очікується виключення IllegalArgumentException
+        printResults(-0.1); // Очікується виключення IllegalArgumentException
+        printResults(0.000001); // Очікується коректний результат
+        printResults(Double.MAX_VALUE);  // Очікується 1
+        printResults(Double.NaN); // Очікується 1
     }
 
     public static double cxx(double epsilon) {
-        if (epsilon <= 0) {
-            throw new IllegalArgumentException("param e = " + epsilon + ", e>0");
+        if (epsilon <= 0) { // Перевірка умови epsilon
+            throw new IllegalArgumentException("Param epsilon = " + epsilon + ", epsilon>0"); // Видаємо помилку
         }
         double sum = 0;
         double term;
         int i = 1;
         do {
             term = 1.0 / (i * i);
+            sum += term;
             i++;
-        } while (Math.abs(term)>=epsilon);
+        } while (Math.abs(term) >= epsilon); // Виконуємо поки діє умова
         return sum;
     }
 
     static void printResults(double epsilon) {
-        System.out.print("e: " + epsilon + " ");
-        try {
+        System.out.println("Epsilon: " + epsilon + " ");
+        try { // Відстежуємо помилку
             System.out.println("Result: " + cxx(epsilon));
         } catch (IllegalArgumentException e) {
             System.out.println("EXCEPTION! " + e.getMessage());
