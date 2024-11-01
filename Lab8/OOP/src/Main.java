27bb305c2cba9aa968330934f79b45ffbeaf2b1f
voloshmiak <voloshenkoyolmishayol@gmail.com>
2024-11-01 12:11:59 +0200

    edited README.md

--- Lab8/OOP/src/Main.java
+++ Lab8/OOP/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        VideoClip mv = new VideoClip("Dont blame me", "Taylor Swift", 266);
-        System.out.println(mv);
+        VideoClip vc = new VideoClip("Dont blame me", "Taylor Swift", 266);
+        System.out.println(vc);
     }
 }
